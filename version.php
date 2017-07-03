2e7d7701e948320da043608075ae3e2da0bea047
Hittesh Ahuja <ha386@bath.ac.uk>
2017-07-03 10:54:54 +0100

    Changed component name to match

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017020902;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015111000;        // Requires this Moodle version
-$plugin->component = 'gradereport_transfer'; // Full name of the plugin (used for diagnostics)
+$plugin->component = 'report_transfer'; // Full name of the plugin (used for diagnostics)
