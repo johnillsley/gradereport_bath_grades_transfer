052fb6d2bab88ea3c5275c918a5cf210ecb02ad8
John Illsley <jsi22@bath.ac.uk>
2017-11-28 16:11:44 +0000

    Correction to previous SQL update

--- lib.php
+++ lib.php
@@ -104,9 +104,9 @@ class transfer_report extends \grade_report
             AND sm.default_map = 1
         JOIN {sits_mappings_enrols} me ON me.map_id = sm.id
         JOIN {user_enrolments} ue ON ue.id = me.u_enrol_id -- PROBLEM WITH user_enrolments BEING REMOVED!!!
-        JOIN {user} u ON u.id = ue.userid AND ra.id = me.ra_id
+        JOIN {user} u ON u.id = ue.userid
         JOIN {role_assignments} ra 
-            ON ra.userid = u.id
+            ON ra.userid = u.id AND ra.id = me.ra_id
             AND contextid = :contextid
             AND roleid = 5 /* student role */
             
